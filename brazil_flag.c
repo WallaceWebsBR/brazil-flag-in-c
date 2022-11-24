5c97663a74d2dbcc1d9c1bc24f6a333d84395a39
Wallace Alberto <1320wallace1320@outlook.com>
2022-11-24 00:15:46 -0300

    Create brazil_flag.c
    #RumoAoHexa

---
+++ brazil_flag.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+    int i, j;
+    for(j=0;j<23;j++) { printf("\e[0;32m" "0"); } printf("\n");
+    for(j=0;j<23;j++) { if(j==11) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>9 && j<13) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>8 && j<14) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>7 && j<15) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>6 && j<16) { if (j>9 && j<13) { if(j > 11 && j <13) { printf("\e[0;37m" "0"); } else {printf("\e[0;34m" "0");}} else { printf("\e[0;33m" "0"); }} else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>5 && j<17) { if (j>8 && j<14) { if(j > 10 && j <12) { printf("\e[0;37m" "0"); } else {printf("\e[0;34m" "0");}} else { printf("\e[0;33m" "0"); }} else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>4 && j<18) { if (j>7 && j<15) { if(j > 9 && j <11) { printf("\e[0;37m" "0"); } else {printf("\e[0;34m" "0");}} else { printf("\e[0;33m" "0"); }} else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>5 && j<17) { if (j>8 && j<14) { if(j > 9 && j <11) { printf("\e[0;37m" "0"); } else {printf("\e[0;34m" "0");}} else { printf("\e[0;33m" "0"); }} else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>6 && j<16) { if (j>9 && j<13) { if(j > 10 && j <12) { printf("\e[0;37m" "0"); } else {printf("\e[0;34m" "0");}} else { printf("\e[0;33m" "0"); }} else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>7 && j<15) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>8 && j<14) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j>9 && j<13) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { if(j==11) { printf("\e[0;33m" "0"); } else { printf("\e[0;32m" "0"); } } printf("\n");
+    for(j=0;j<23;j++) { printf("\e[0;32m" "0"); } printf("\n");
+
+    printf("\n BR\e[0;33mAZ\e[0;34mIL \e[0;37mFLAG by WallaceWebs #RUMO_AO_HEXA");
+    return 0;
+}
